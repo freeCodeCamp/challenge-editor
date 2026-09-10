48b3607cfc037df9767508ec3e8ddbbf32412276
majestic-owl448 <26656284+majestic-owl448@users.noreply.github.com>
2026-09-10 13:10:22 +0200

    fix: show readable step tool results (#12)

--- apps/api/routes/tools-route.ts
+++ apps/api/routes/tools-route.ts
@@ -10,19 +10,21 @@ const asyncExec = promisify(exec);
 
 const toolsSwitch: ToolsSwitch = {
   'create-next-step': directory => {
-    return asyncExec(`cd ${directory} && pnpm run create-next-step`);
+    return asyncExec(`cd ${directory} && pnpm --silent run create-next-step`);
   },
   'create-empty-steps': (directory, num) => {
-    return asyncExec(`cd ${directory} && pnpm run create-empty-steps ${num}`);
+    return asyncExec(
+      `cd ${directory} && pnpm --silent run create-empty-steps ${num}`
+    );
   },
   'insert-step': (directory, num) => {
-    return asyncExec(`cd ${directory} && pnpm run insert-step ${num}`);
+    return asyncExec(`cd ${directory} && pnpm --silent run insert-step ${num}`);
   },
   'delete-step': (directory, num) => {
-    return asyncExec(`cd ${directory} && pnpm run delete-step ${num}`);
+    return asyncExec(`cd ${directory} && pnpm --silent run delete-step ${num}`);
   },
   'update-step-titles': directory => {
-    return asyncExec(`cd ${directory} && pnpm run update-step-titles`);
+    return asyncExec(`cd ${directory} && pnpm --silent run update-step-titles`);
   }
 };
 
